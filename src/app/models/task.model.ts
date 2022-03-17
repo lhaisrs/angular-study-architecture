1cfa36c4d513b9bf58ca778f4d5face2822c6085
lhais.silva <lhais.silva@tranquilidade.pt>
2022-03-17 18:47:16 +0000

    setup view HTML/CSS

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,13 +1,15 @@
 export class FactModel {
     private readonly id: string;
     public readonly createdAt: string;
-    public readonly text: string;
+    public readonly description: string;
     public readonly updatedAt: string;
+    public readonly completed: boolean;
 
-    constructor(_id: string, _createdAt: string, _text: string, _updatedAt: string) {
+    constructor(_id: string, _createdAt: string, _description: string, _updatedAt: string, _completed: boolean) {
         this.id = _id;
         this.createdAt = _createdAt;
-        this.text = _text;
+        this.description = _description;
         this.updatedAt = _updatedAt;
+        this.completed = _completed;
     }
 }
